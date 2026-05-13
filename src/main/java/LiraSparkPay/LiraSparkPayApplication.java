4e04fc4856194b30dbef7e49f2cf2ebe9b9a3f0e
Jhonatan <jhonatanlira@gmail.com>
2026-05-13 12:51:46 -0300

    Begin

---
+++ src/main/java/LiraSparkPay/LiraSparkPayApplication.java
@@ -0,0 +1,13 @@
+package LiraSparkPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiraSparkPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiraSparkPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LiraSparkPay/LiraSparkPayApplicationTests.java
@@ -0,0 +1,13 @@
+package LiraSparkPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiraSparkPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
